e35cb3408b62af35d05f348b196937e24c308659
zhshuvo <zhshuvo99@gmail.com>
2023-07-26 21:17:05 +0600

    implemented add product api

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -11,3 +11,30 @@ export const GET = async () => {
         products,
     });
 };
+
+export const POST = async (req) => {
+    const body = await req.json();
+
+    await startDB();
+
+    try {
+        // Create a new product in the database
+        const product = await ProductModel.create({ ...body });
+
+        return NextResponse.json({
+            product: {
+                name: product.name,
+                description: product.description,
+                img: product.img,
+                price: product.price,
+            },
+        });
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: "Error creating the product",
+            },
+            { status: 500 }
+        );
+    }
+};
